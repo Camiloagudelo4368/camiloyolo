7a86569e38994801874a7e1e309d85808d99c966
Camilo Agudelo <camilo@Camilos-MacBook-Pro-2.local>
2019-07-03 00:54:28 -0400

    Updates

--- precache-manifest.89e99ab3a541182403f9d969c9429a32.js
+++ precache-manifest.89e99ab3a541182403f9d969c9429a32.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/camiloyolo/static/js/runtime~main.c52c862b.js"
   },
   {
-    "revision": "383cf29ccf5e5349fd0b",
-    "url": "/camiloyolo/static/js/main.f1cbc916.chunk.js"
+    "revision": "2e1b6d2686b4f5e5870b",
+    "url": "/camiloyolo/static/js/main.ae09cbc5.chunk.js"
   },
   {
     "revision": "d573435bdc5baa9218b0",
     "url": "/camiloyolo/static/js/2.5b0a2b8e.chunk.js"
   },
   {
-    "revision": "383cf29ccf5e5349fd0b",
+    "revision": "2e1b6d2686b4f5e5870b",
     "url": "/camiloyolo/static/css/main.75baf46e.chunk.css"
   },
   {
-    "revision": "ef58082c25bd0c25cae3def47a4efa9c",
+    "revision": "fe58c3bc19a0c4c5608460b3e178aad2",
     "url": "/camiloyolo/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/camiloyolo/precache-manifest.a464aecf10823c6f9820b6b0400b7147.js"
+  "/camiloyolo/precache-manifest.89e99ab3a541182403f9d969c9429a32.js"
 );
 
 workbox.clientsClaim();
