0fbb3d76f71fa713436178831a563b168a82d445
Camilo Agudelo <camilo@Camilos-MacBook-Pro-2.local>
2019-07-04 12:18:31 -0400

    Updates

---
+++ precache-manifest.d297abb8ca41f0de2db6db23e8a52cca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "baa0a04a3098cffa6313",
+    "url": "/camiloyolo/static/js/runtime~main.c52c862b.js"
+  },
+  {
+    "revision": "3f3b76c33fecfeedc26b",
+    "url": "/camiloyolo/static/js/main.6f67b0d4.chunk.js"
+  },
+  {
+    "revision": "6424598cf2112d262fc6",
+    "url": "/camiloyolo/static/js/2.e02a8b48.chunk.js"
+  },
+  {
+    "revision": "3f3b76c33fecfeedc26b",
+    "url": "/camiloyolo/static/css/main.75baf46e.chunk.css"
+  },
+  {
+    "revision": "ec5dc2c58c5ce3c87d86f2551a9b025f",
+    "url": "/camiloyolo/index.html"
+  }
+];
