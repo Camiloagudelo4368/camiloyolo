7fda860e584315be8550bac15448bc14a830c2d3
Camilo Agudelo <camilo@Camilos-MacBook-Pro-2.local>
2019-07-04 23:02:30 -0400

    fixing routes

--- index.js
+++ index.js
@@ -5,40 +5,17 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// app.use(cors({
-//   origin: 'http://localhost',
-//   allowedHeaders: ["Content-Type", "Authorization"]
-// }));
-
-// app.options('*', cors())
-
-
-// app.all('', function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost");
-//   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   //Auth Each API Request created by user.
-//   next();
-// });
-
 console.log('port' + PORT);
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Add routes, both API and view
 app.use(routes);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/YOLO", { useNewUrlParser: true });
 
